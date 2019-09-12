fedd4b90e900948e5669fd8fcbdb0b61a1acfeda
enderson <enderson19952009@gmail.com>
2019-09-12 10:39:18 -0400

    start arduino

---
+++ Codigo_arduino/Codigo_arduino.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // put your setup code here, to run once:
+  // code prube conection with mango automation
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
