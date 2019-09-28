f89bf2a5ef3121071f470c160d1de61fa3b62f3e
enderson <enderson19952009@gmail.com>
2019-09-27 21:44:01 -0400

    gitflow-feature-stash: ino
    start arduino code

--- Codigo_arduino/Codigo_arduino.ino
+++ Codigo_arduino/Codigo_arduino.ino
@@ -1,10 +1,21 @@
+#include <ArduinoJson.h>
+
+
+int cont=0;
+
 void setup() {
   // put your setup code here, to run once:
   // code prube conection with mango automation
+  Serial.begin(9600);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.print(" Contador: ");
+  Serial.print(cont);
+  Serial.write("hola");
 
+  cont++;
+  delay(1000);
 }
